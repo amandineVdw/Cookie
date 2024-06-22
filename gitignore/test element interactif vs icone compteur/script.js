64db2c6567d2aeeab8f3a5fc23b64ccc6d36f8d7
Amandine Van de Winckel <164058998+amandineVdw@users.noreply.github.com>
2024-06-22 16:06:35 +0200

    pre-code cookie ok

--- gitignore/test element interactif vs icone compteur/script.js
+++ gitignore/test element interactif vs icone compteur/script.js
@@ -1,24 +1,23 @@
 var Game = {
-    score: 0,
-    clickLump: function(element) {
-      this.score++;
-      this.updateScore();
-      this.showFloatingText(element, '+1');
-    },
-    updateScore: function() {
-      var lumpsAmount = document.getElementById('lumpsAmount');
-      lumpsAmount.innerText = this.score;
-    },
-    showFloatingText: function(element, text) {
-      var floatingText = document.createElement('div');
-      floatingText.className = 'floatingText';
-      floatingText.innerText = text;
-  
-      element.appendChild(floatingText);
-  
-      setTimeout(function() {
-        element.removeChild(floatingText);
-      }, 1000); // Remove floating text after 1 second
-    }
-  };
-  
+  score: 0,
+  clickLump: function (element) {
+    this.score++;
+    this.updateScore();
+    this.showFloatingText(element, "+1");
+  },
+  updateScore: function () {
+    var lumpsAmount = document.getElementById("lumpsAmount");
+    lumpsAmount.innerText = this.score;
+  },
+  showFloatingText: function (element, text) {
+    var floatingText = document.createElement("div");
+    floatingText.className = "floatingText";
+    floatingText.innerText = text;
+
+    element.appendChild(floatingText);
+
+    setTimeout(function () {
+      element.removeChild(floatingText);
+    }, 1000); // Remove floating text after 1 second
+  },
+};
