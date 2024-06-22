d2ad81a76ae6bbb4d210fd903869db55279cc2e6
Amandine Van de Winckel <164058998+amandineVdw@users.noreply.github.com>
2024-06-22 15:36:59 +0200

    draft almost ok code

---
+++ gitignore/test element interactif vs icone compteur/script.js
@@ -0,0 +1,24 @@
+var Game = {
+    score: 0,
+    clickLump: function(element) {
+      this.score++;
+      this.updateScore();
+      this.showFloatingText(element, '+1');
+    },
+    updateScore: function() {
+      var lumpsAmount = document.getElementById('lumpsAmount');
+      lumpsAmount.innerText = this.score;
+    },
+    showFloatingText: function(element, text) {
+      var floatingText = document.createElement('div');
+      floatingText.className = 'floatingText';
+      floatingText.innerText = text;
+  
+      element.appendChild(floatingText);
+  
+      setTimeout(function() {
+        element.removeChild(floatingText);
+      }, 1000); // Remove floating text after 1 second
+    }
+  };
+  
